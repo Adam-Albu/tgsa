6f3ff4cb376f6e4fe1a081bd6380080ab5f7c9a9
Adam-Albu <91206929+Adam-Albu@users.noreply.github.com>
2022-03-10 16:31:26 +0000

    Update pxt.json, main.blocks, main.ts, main.py, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts, setup.ts

---
+++ images.g.ts
@@ -0,0 +1,94 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "J+t-Qo6PXa_)atail4XH":
+            case "spawn":return img`
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 f f 7 f f 7 7 f f f 7 7 7 
+7 7 f 7 7 7 f 7 f 7 f 7 f 7 7 7 
+7 7 7 f 7 7 f f 7 7 f 7 f 7 7 7 
+7 7 7 7 f 7 f 7 7 7 f f f 7 7 7 
+7 7 f f 7 7 f 7 7 7 f 7 f 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 f 7 7 7 f 7 f f 7 7 f 7 7 7 
+7 7 f 7 7 7 f 7 f 7 f 7 f 7 7 7 
+7 7 f 7 7 7 f 7 f 7 f 7 f 7 7 7 
+7 7 f 7 f 7 f 7 f 7 f 7 f 7 7 7 
+7 7 7 f 7 f 7 7 f 7 7 f f 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+`;
+            case "image2":
+            case "grass":return img`
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+`;
+            case "image1":
+            case "lava":return img`
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+`;
+            case "image3":
+            case "goal":return img`
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 f f f 2 2 2 2 2 2 2 f f 2 2 
+2 f 2 2 2 2 2 2 2 2 2 f 2 2 f 2 
+2 f 2 2 2 2 2 2 2 2 2 f 2 2 f 2 
+2 f 2 2 2 2 2 2 2 2 2 f 2 2 f 2 
+2 f 2 2 f 2 2 2 2 2 2 f 2 2 f 2 
+2 f 2 2 f 2 2 2 2 2 2 f 2 2 f 2 
+2 f f f f 2 2 2 2 2 2 2 f f 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 f f f f 2 2 2 2 2 2 f 2 2 2 2 
+2 f 2 2 f 2 2 2 2 2 2 f 2 2 2 2 
+2 f 2 2 f 2 2 2 2 2 2 f 2 2 2 2 
+2 f 2 2 f 2 2 2 2 2 2 f 2 2 2 2 
+2 f f f f 2 2 2 2 2 2 f 2 2 2 2 
+2 f 2 2 f 2 2 2 2 2 2 f f f f 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
--- main.ts
+++ main.ts
@@ -1,1 +1,6 @@
- 
+function setup () {
+    scene.setTile(2, assets.image`lava`, false)
+    scene.setTile(14, assets.image`grass`, false)
+    scene.setTile(7, assets.image`spawn`, false)
+    scene.setTile(15, assets.image`goal`, false)
+}
